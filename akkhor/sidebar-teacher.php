b269c9781d776a5ed04052ee3b442ae8efeb7e18
kasrarh <kasra.rahimian@gmail.com>
2019-10-15 17:34:34 +0330

    dashboard uploaded

---
+++ akkhor/sidebar-teacher.php
@@ -0,0 +1,237 @@
+<body>
+<!-- Preloader Start Here -->
+<div id="preloader"></div>
+<!-- Preloader End Here -->
+<div id="wrapper" class="wrapper bg-ash">
+    <!-- Header Menu Area Start Here -->
+    <div class="navbar navbar-expand-md header-menu-one bg-light">
+        <div class="nav-bar-header-one">
+            <div class="header-logo">
+                <a href="index.html">
+                    <img src="img/yellow.jpg" alt="logo">
+                </a>
+            </div>
+            <div class="toggle-button sidebar-toggle">
+                <button type="button" class="item-link">
+                        <span class="btn-icon-wrap">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </span>
+                </button>
+            </div>
+        </div>
+        <div class="d-md-none mobile-nav-bar">
+            <button class="navbar-toggler pulse-animation" type="button" data-toggle="collapse" data-target="#mobile-navbar" aria-expanded="false">
+                <i class="far fa-arrow-alt-circle-down"></i>
+            </button>
+            <button type="button" class="navbar-toggler sidebar-toggle-mobile">
+                <i class="fas fa-bars"></i>
+            </button>
+        </div>
+        <div class="header-main-menu collapse navbar-collapse" id="mobile-navbar">
+            <ul class="navbar-nav">
+                <li class="navbar-item header-search-bar">
+                    <div class="input-group stylish-input-group">
+                            <span class="input-group-addon">
+                                <button type="submit">
+                                    <span class="flaticon-search" aria-hidden="true"></span>
+                                </button>
+                            </span>
+                        <input type="text" class="form-control" placeholder="پیدا کردن چیزی . . .">
+                    </div>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="navbar-item dropdown header-admin">
+                    <a class="navbar-nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                       aria-expanded="false">
+                        <div class="admin-title">
+                        </div>
+                        <div class="admin-img">
+                            <img src="img/figure/admin.jpg" alt="Admin">
+                        </div>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <div class="item-header">
+                            <h6 class="item-title">سنترال فایل</h6>
+                        </div>
+                        <div class="item-content">
+                            <ul class="settings-list">
+                                <li><a href="#"><i class="flaticon-user"></i>پروفایل من</a></li>
+                                <li><a href="#"><i class="flaticon-list"></i>وظیفه</a></li>
+                                <li><a href="#"><i class="flaticon-chat-comment-oval-speech-bubble-with-text-lines"></i>پیام</a></li>
+                                <li><a href="#"><i class="flaticon-gear-loading"></i>تنظیمات حساب</a></li>
+                                <li><a href="#"><i class="flaticon-turn-off"></i>خروج</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+                <li class="navbar-item dropdown header-message">
+                    <a class="navbar-nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                       aria-expanded="false">
+                        <i class="far fa-envelope"></i>
+                        <div class="item-title d-md-none text-16 mg-l-10">پیام</div>
+                        <span>5</span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <div class="item-header">
+                            <h6 class="item-title">05 پیام</h6>
+                        </div>
+                        <div class="item-content">
+                            <div class="media">
+                                <div class="item-img bg-skyblue author-online">
+                                    <img src="img/figure/student11.png" alt="img">
+                                </div>
+                                <div class="media-body space-sm">
+                                    <div class="item-title">
+                                        <a href="#">
+                                            <span class="item-name">سنترال فایل</span>
+                                            <span class="item-time">18:30</span>
+                                        </a>
+                                    </div>
+                                    <p>دلیل خرید این مورد چیست.
+                                        آیا برای من مفید است.....</p>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <div class="item-img bg-yellow author-online">
+                                    <img src="img/figure/student12.png" alt="img">
+                                </div>
+                                <div class="media-body space-sm">
+                                    <div class="item-title">
+                                        <a href="#">
+                                            <span class="item-name">سنترال فایل</span>
+                                            <span class="item-time">10:35</span>
+                                        </a>
+                                    </div>
+                                    <p>دلیل خرید این مورد چیست.
+                                        آیا برای من مفید است.....</p>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <div class="item-img bg-pink">
+                                    <img src="img/figure/student13.png" alt="img">
+                                </div>
+                                <div class="media-body space-sm">
+                                    <div class="item-title">
+                                        <a href="#">
+                                            <span class="item-name">آرش </span>
+                                            <span class="item-time">02:35</span>
+                                        </a>
+                                    </div>
+                                    <p>دلیل خرید این مورد چیست.
+                                        آیا برای من مفید است.....</p>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <div class="item-img bg-violet-blue">
+                                    <img src="img/figure/student11.png" alt="img">
+                                </div>
+                                <div class="media-body space-sm">
+                                    <div class="item-title">
+                                        <a href="#">
+                                            <span class="item-name">سنترال فایل</span>
+                                            <span class="item-time">12:35</span>
+                                        </a>
+                                    </div>
+                                    <p>دلیل خرید این مورد چیست.
+                                        آیا برای من مفید است.....</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="navbar-item dropdown header-notification">
+                    <a class="navbar-nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                       aria-expanded="false">
+                        <i class="far fa-bell"></i>
+                        <div class="item-title d-md-none text-16 mg-l-10">اطلاعیه</div>
+                        <span>8</span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <div class="item-header">
+                            <h6 class="item-title">03 اطلاعیه</h6>
+                        </div>
+                        <div class="item-content">
+                            <div class="media">
+                                <div class="item-icon bg-skyblue">
+                                    <i class="fas fa-check"></i>
+                                </div>
+                                <div class="media-body space-sm">
+                                    <div class="post-title">تکمیل کار امروز</div>
+                                    <span>1 دقیقه پیش</span>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <div class="item-icon bg-orange">
+                                    <i class="fas fa-calendar-alt"></i>
+                                </div>
+                                <div class="media-body space-sm">
+                                    <div class="post-title">جلسه مدیر</div>
+                                    <span>20 دقیقه پیش</span>
+                                </div>
+                            </div>
+                            <div class="media">
+                                <div class="item-icon bg-violet-blue">
+                                    <i class="fas fa-cogs"></i>
+                                </div>
+                                <div class="media-body space-sm">
+                                    <div class="post-title">به روز رسانی رمز عبور</div>
+                                    <span>45 دقیقه پیش</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="navbar-item dropdown header-language">
+                    <a class="navbar-nav-link dropdown-toggle" href="#" role="button"
+                       data-toggle="dropdown" aria-expanded="false"><i class="fas fa-globe-americas"></i>ان</a>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a class="dropdown-item" href="#">انگلیسی</a>
+                        <a class="dropdown-item" href="#">اسپانیایی</a>
+                        <a class="dropdown-item" href="#">فرانسوی</a>
+                        <a class="dropdown-item" href="#">چینی</a>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- Header Menu Area End Here -->
+    <!-- Page Area Start Here -->
+    <div class="dashboard-page-one">
+        <!-- Sidebar Area Start Here -->
+        <div class="sidebar-main sidebar-menu-one sidebar-expand-md sidebar-color">
+            <div class="mobile-sidebar-header d-md-none">
+                <div class="header-logo">
+                    <a href="index.html"><img src="img/logo1.png" alt="logo"></a>
+                </div>
+            </div>
+            <div class="sidebar-menu-content">
+                <ul class="nav nav-sidebar-menu sidebar-toggle-view">
+                    <li class="nav-item sidebar-nav-item">
+                        <a href="#" class="nav-link"><i
+                                class="flaticon-multiple-users-silhouette"></i><span>معلمان</span></a>
+                        <ul class="nav sub-group-menu">
+                            <li class="nav-item">
+                                <a href="teacher-class.html" class="nav-link"><i class="fas fa-angle-right"></i>کلاس ها</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="teacher-attendance.html" class="nav-link"><i
+                                        class="fas fa-angle-right"></i>لیست حضوری</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="add-book.html" class="nav-link"><i class="fas fa-angle-right"></i>جزوات و کتاب ها</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="teacher-exam.html" class="nav-link"><i
+                                        class="fas fa-angle-right"></i>امتحانات</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- Sidebar Area End Here -->
